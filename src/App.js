fc3e4776a67fc1385d3c8bd5085311a3ebf07d32
Rajasegar Chandran <rajasegar.c@gmail.com>
2020-11-18 17:48:49 +0530

    chore: first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import htm from 'htm';
+const html = htm.bind(React.createElement);
+
+export default function App() {
+    return html`
+      <div>
+        <h1>Hello, World!</h1>
+      </div>
+    `;
+}
